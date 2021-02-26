772f5cd8bada6f390ca9f6b99dfccd64d1f4c6b6
Erkki Saaremets <erkki.saaremets@mail.ee>
2021-02-26 11:24:05 +0200

    new stuff

---
+++ lambda.py
@@ -0,0 +1,8 @@
+# def f(x):
+#     return 3*x + 1
+
+# print(f(2))
+
+g = lambda x: 3*x + 1 #anonymous function which can be embedded instead of just called
+
+print(g)
---
+++ sigle-set.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot as plt 
+from collections import Counter
+
+num_friends = [100,49,41,40,25]
+
+friend_counts = Counter(num_friends)
+#print(friend_counts)
+xs = range(101)
+ys = [friend_counts[x] for x in xs]
+plt.bar(xs,ys)
+plt.axis([0, 101, 0, 25])
+plt.title("Histogram of friend counts")
+plt.xlabel("# of friends")
+plt.ylabel("# of people")
+
+plt.show()
